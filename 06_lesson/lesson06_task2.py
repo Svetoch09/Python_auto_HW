8f6bc517bc48d07019a6317227448df204de6aeb
Svetlana Paskevich <strays-binges.4d@icloud.com>
2025-10-14 13:59:20 +0200

    lesson 6, selenium, work with elements, impl+expl.wait

---
+++ 06_lesson/lesson06_task2.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+# Setup
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager()
+                                                .install()))
+
+driver.get("http://uitestingplayground.com/textinput")
+# --- Actions ---
+# 1. Define locators/input
+input_text_locator = "#newButtonName"  # CSS Selector for the input field
+button_locator = "#updatingButton"     # CSS Selector for the button
+
+# 2. Type text into the input field
+input_field = driver.find_element(By.ID, "newButtonName")
+input_field.send_keys("SkyPro")
+
+# 3. Click the button to update its name
+click_button = driver.find_element(By.ID, "updatingButton")
+click_button.click()
+
+# --- Wait and Verification ---
+driver.implicitly_wait(3)
+print(driver.find_element(By.ID, "updatingButton").text)
+driver.quit()
