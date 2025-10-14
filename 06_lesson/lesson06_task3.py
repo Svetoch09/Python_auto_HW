02cdeacbd07fe5d806f580e8087b53875390f094
Svetlana Paskevich <strays-binges.4d@icloud.com>
2025-10-14 18:05:30 +0200

    lesson 6, selenium, work with elements, impl+expl.wait

--- 06_lesson/lesson06_task3.py
+++ 06_lesson/lesson06_task3.py
@@ -12,9 +12,11 @@ driver.get("https://bonigarcia.dev/selenium-webdriver-java/"
            "loading-images.html")
 waiter = WebDriverWait(driver, 10, 0.5)
 
-picture_locator = "#image-container [alt = 'award']"
-waiter.until(
-    EC.visibility_of_element_located((By.CSS_SELECTOR, picture_locator)))
+last_picture_locator = "#image-container img:nth-of-type(4)"
+picture_locator = "#image-container img:nth-child(3)"
+all_pictures = waiter.until(
+    EC.visibility_of_all_elements_located((By.CSS_SELECTOR, last_picture_locator))
+)
 
 pic_attribute = (driver.find_element(By.CSS_SELECTOR, picture_locator)
                  .get_attribute("src"))
